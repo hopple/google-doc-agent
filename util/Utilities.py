28f5ca8e54c9cbfbb7f490566d179383c58d3a6d
Ganggao Zhu <zhuganggao@gmail.com>
2013-10-02 14:01:28 +0200

    add a function for sending mail

--- util/Utilities.py
+++ util/Utilities.py
@@ -1,3 +1,6 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 from urllib import urlopen
 
 url_ip = "http://api.externalip.net/ip"
@@ -8,3 +11,27 @@ def getIP():
     """
     ip = urlopen(url_ip).read()
     return ip
+
+def sendEmail(from_addr, to_addr_list, cc_addr_list,
+        subject, message, username, password,
+        smtpserver='smtp.gmail.com:587'):
+    """
+    send a mail.
+    """
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = ','.join(to_addr_list)
+    msg['Cc'] = ','.join(cc_addr_list)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+    server = smtplib.SMTP(smtpserver)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(username, password)
+    text = msg.as_string()
+    problems = server.sendmail(from_addr, to_addr_list, text)
+    server.quit()
+    return problems
+
